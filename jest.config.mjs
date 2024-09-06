ccf812a482fab46bf8b696b984953726650a983d
Nicolas Martinez V <nmartinezv@icloud.com>
2024-09-06 00:10:22 -0400

    feat: coverage and jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,11 +1,50 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { readFileSync } from 'fs';
 
-export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  { ignores: ['coverage/**/*', 'lib/**/*'] },
-];
+const { compilerOptions } = JSON.parse(readFileSync('./tsconfig.json', 'utf8'));
+
+export default {
+  projects: [
+    {
+      displayName: 'Unit Test',
+      moduleDirectories: ['node_modules', __dirname],
+      moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+      collectCoverageFrom: ['src/**/*'],
+      coverageThreshold: {
+        global: {
+          branches: 90,
+          functions: 90,
+          lines: 90,
+          statements: 90,
+        },
+      },
+      clearMocks: true,
+      coverageDirectory: 'coverage',
+      coveragePathIgnorePatterns: [
+        '/node_modules/',
+        '/__fixtures__/',
+        '/(__)?test(s__)?/',
+        '/(__)?mock(s__)?/',
+        '/__jest__/',
+        '.?.min.js',
+        'interfaces.ts',
+      ],
+      coverageProvider: 'v8',
+      presets: 'ts-jest',
+      rootDir: 'src',
+      setupFilesAfterEnv: ['dotenv/config'],
+      testEnvironment: 'node',
+      testRegex: '\\.spec\\.ts$',
+    },
+    {
+      displayName: 'E2E Test',
+      moduleDirectories: ['node_modules', __dirname],
+      moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+      preset: 'ts-jest',
+      rootDir: 'test',
+      setupFilesAfterEnv: ['dotenv/config'],
+      testEnvironment: 'node',
+      testRegex: '\\.end2end.spec\\.ts$',
+    },
+  ],
+};
